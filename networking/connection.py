6d04b65b6b158f6f4a17df3df3c37563e504607b
Johan Ortega <hal215@outlook.com>
2022-01-08 00:46:27 -0600

    attempted connections to invalid hosts are now handled in Client and view
    If the host is valid but does not respond to client's request within 15 seconds, a 
    
    Created:
    networking/errors.py
       - contains exceptions pertaining to critical errors, may contain more in the future
       - currently only contains InvalidHostError
    
    Modified:
    ui/view.py
       - Added exception handling in dispatch_search to listen for requests to invalid hosts (InvalidHostError)
          + displays `invalid_host_page` instead if exception occurs
       - Moved C-Q, C-S keybinds higher up in application priority
          + it didn't make sense for them to be lower in priority than keystrokes to the search bar
     
    networking/connection.py
       - Added exception handling to constructor when connecting to specified host, handles socket level exceptions
          + If an exception is detected, `InvalidHostError` is raised
       - Added a timeout to `receive_response`, in case the server either doesn't want to talk to us, or is not setup for the gemini protocol

--- networking/connection.py
+++ networking/connection.py
@@ -1,5 +1,7 @@
-import ssl, socket
+import ssl, socket, select
 import networking.lagcerts as lagcerts
+from networking.errors import InvalidHostError
+
 from datetime import datetime, timezone
 from OpenSSL import crypto
 from cryptography import x509
@@ -16,17 +18,22 @@ class Response:
     body: bytes
 
 
-
 #TODO: open connection to the host, and ask for the particular page when making the request
 #      e.g. open socket to host 'gemini.circumlunar.space'
 #      but when making the request, request 'gemini://gemini.circumlunar.space/'
 class Connection:
 
+
     def __init__(self, URL, cert=None):
         # make connection
         context = ssl.SSLContext()
         self.host, self.query = self._parse_query(URL)
-        connection = socket.create_connection((self.host, GEMINI_PORT))
+
+        try:
+            connection = socket.create_connection((self.host, GEMINI_PORT), 20)
+        except socket.error:
+            raise InvalidHostError
+
         self.sock = context.wrap_socket(connection)
         der_cert = self.sock.getpeercert(True)
         pem_cert = ssl.DER_cert_to_PEM_cert(der_cert)
@@ -68,14 +75,16 @@ class Connection:
     def receive_response(self):
         PACKET_SIZE = 2048
         headers = self.sock.recv(PACKET_SIZE)
-        print(headers)
-        print("I am in receive_response")
+        #print(headers)
+        #print("I am in receive_response")
         body = b''
         buf = b'ohnoo'
 
-        while len(buf) != 0:
-            buf = self.sock.recv(PACKET_SIZE)
-            body += buf
+        ready = select.select([self.sock], [], [], 10)
+        if ready[0]:
+            while len(buf) != 0:
+                buf = self.sock.recv(PACKET_SIZE)
+                body += buf
 
         self.sock.close()
         return Response(headers[0:2], headers[3:len(headers)-2], body)
