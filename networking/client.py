d887a33e7b2ca2cbf9c7589e45434d093f34b82e
Johan Ortega <hal215@outlook.com>
2022-01-09 05:06:45 -0600

    Lagcerts now returns properly if certificates match what's been stored
    Fixes #6
    
    Modified:
    networking/lagcerts.py
       - Added logging statements in `valid_cert()`
       - Added an else statement for the case where the fingerprints match
     
    networking/client.py
       - Removed the input line from the if statement proceeding the call to `valid_cert`
          - This was causing the app to lock up, the only way to unlock your terminal would be to kill the process. Causing the the app to stop before the UI library has a change to release control of the screen, making you print a bunch of gibberish to the terminal when you move your mouse in its viewport. 
       - Now passes the host to `valid_cert` rather than the whole URL

--- networking/client.py
+++ networking/client.py
@@ -2,6 +2,8 @@ from networking import lagcerts
 from networking.connection import Connection, Response
 from networking.errors import InvalidHostError
 
+from networking.log import netlog
+
 invalid_host_page = b'# Host not found\nPlease make sure the address is correct.\n'
 
 class Client:
@@ -11,14 +13,7 @@ class Client:
         self.cert = lagcerts.generate_client_cert()
 
     def prompt_for_safety():
-        # prompt user using ncurses
-        if user_in == "Y" or user_in == "y":
-            return 1
-        elif user_in == "N" or user_in == "n":
-            return 0
-        else:
-            # tell user to use right response
-            return 0
+        return 0
 
     def _make_dummy_response(self, body):
         return Response("20", b"gemini/text", body)
@@ -28,8 +23,8 @@ class Client:
             page_conn = Connection(URL)
 
             if page_conn.need_to_prompt():
-                if input("Unsafe maybe, continue? y/n: ") != 'y':
-                    return 1
+                netlog.debug("Unsafe")
+
 
             page_conn.send_request()
             #print("Receiving response")
